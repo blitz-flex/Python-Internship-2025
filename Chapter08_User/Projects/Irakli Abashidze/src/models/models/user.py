9d96b576529418a3e022082b630dbab79812afa6
Flex <abashidzeikaa@gmail.com>
2025-06-11 02:40:41 +0400

    config  model

---
+++ Chapter08_User/Projects/Irakli Abashidze/src/models/models/user.py
@@ -0,0 +1,29 @@
+from ..extensions import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class UserAccount(db.Model, UserMixin):
+    __tablename__ = 'user_accounts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    birthdate = db.Column(db.Date, nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    profile_image = db.Column(db.String(255), nullable=True)
+
+    # backref-ის ნაცვლად ვიყენებთ back_populates-ს, რაც ორმხრივ კავშირს უზრუნველყოფს
+    registrations = db.relationship('ProgramRegistration', back_populates='user', cascade='all, delete-orphan')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return UserAccount.query.get(int(user_id))
